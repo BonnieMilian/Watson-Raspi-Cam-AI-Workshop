28ad33fed565b25f85e46cee735811496d96fc26
BonnieMilian <bonniemilian@galileo.edu>
2017-10-10 10:50:48 -0600

    Camara

--- raspiCam/camera.js
+++ raspiCam/camera.js
@@ -1,5 +1,3 @@
-var five = require("johnny-five");
-var Raspi = require("raspi-io");
 var RaspiCam = require("raspicam");
 
 var camera = new RaspiCam({
@@ -7,34 +5,6 @@ var camera = new RaspiCam({
   output: "picture.jpg",
   timeout: 500
 });
-
-var board = new five.Board({
-  io: new Raspi()
-});
-
-var button;
-
-board.on("ready", () => {
-  button = new five.Button({
-    pin: "GPIO18",
-    isPullup: true
-  });
-
-  button.on("down", (value) => {
-    var time = new Date().getTime();
-
-    camera.set( "output", "picture_" + time +".jpg" );
-
-    camera.start();
-
-    console.log("Pressed");
-  });
-
-  button.on("up", () => {
-    console.log("Released");
-  });
-});
-
 
 camera.on("read", (error, timestamp, filename) => {
   if(error)
@@ -45,3 +15,5 @@ camera.on("read", (error, timestamp, filename) => {
 camera.on("exit", (timestamp) => {
   console.log("Finished");
 });
+
+camera.start();
